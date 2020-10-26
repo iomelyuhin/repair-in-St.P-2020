9281b2d1622dfd71cfbcf47dd647c658f11f95be
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2020-10-27 00:36:16 +0300

    add variables page, fix css bags for mobiles +

--- gulp/tasks/imageToWebp.js
+++ gulp/tasks/imageToWebp.js
@@ -1,23 +1,8 @@
 const gulp = require('gulp')
-const imagemin = require('gulp-imagemin')
 const webp = require('gulp-webp')
 
-module.exports = function imageMinify() {
+module.exports = function imageToWebp() {
   return gulp.src('src/assets/img/content/*.{gif,png,jpg,svg,webp}')
-    .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({
-        quality: 85,
-        progressive: true
-      }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
-    ]))
     .pipe(webp())
     .pipe(gulp.dest('build/assets/img/content/'))
 }
